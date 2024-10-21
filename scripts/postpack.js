60df445277cf907933221b1a4e7d4f5ebf22e7f9
Kim Brose <kim.brose@nordeck.net>
2024-10-21 10:06:31 +0200

    prettier
    Signed-off-by: Kim Brose <kim.brose@nordeck.net>

--- scripts/postpack.js
+++ scripts/postpack.js
@@ -6,6 +6,6 @@
 const PACKAGE_PATH = 'package.json';
 const PACKAGE_PATH_BACKUP = 'package.json-prepack';
 
-import * as fs from "fs";
+import * as fs from 'fs';
 
 fs.renameSync(PACKAGE_PATH_BACKUP, PACKAGE_PATH);
--- scripts/publishAllPackages.js
+++ scripts/publishAllPackages.js
@@ -4,9 +4,9 @@
  * !!! This script is intended to only be run by the github workflow! !!!
  */
 
-import * as fs from "fs";
-import * as path from "path";
-import * as child_process from "child_process";
+import * as child_process from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const packages = fs.readdirSync('packages');
 
